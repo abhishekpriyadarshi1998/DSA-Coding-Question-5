528316f517835a84a287e61bbeebd90ec87e01ab
abhishekpriyadarshi1998 <115804419+abhishekpriyadarshi1998@users.noreply.github.com>
2022-12-08 01:46:41 +0530

    Add files via upload

---
+++ DSA Coding Question 5.py
@@ -0,0 +1,10 @@
+def cyclicallyRotate(arr, n): 
+   temp = arr[n-1] 
+   for i in range(n-1, 0, -1): 
+      arr[i] = arr[i-1] 
+   arr[0] = temp 
+   return arr 
+
+arr = [1, 2, 3, 4, 5] 
+n = len(arr) 
+print(cyclicallyRotate(arr, n))
